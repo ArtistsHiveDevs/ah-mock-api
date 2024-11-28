b3d7c888f6dd702b43d265810a2d63c646230c58
Carlos Navarrete <npcarlos@users.noreply.github.com>
2024-11-28 12:33:17 -0500

    creacion de eventos reales

--- db/modify.js
+++ db/modify.js
@@ -1,12 +1,24 @@
 const { default: mongoose } = require("mongoose");
 const User = require("../models/appbase/User");
+var fs = require('fs');
+
 const connectToDatabase = require("./db");
 async function main() {
   try {
     await connectToDatabase();
     console.log("Conectado... ");
     // Ejecuta el script
-    await User.updateMany({}, { $set: { roles: [] } });
+    // await User.updateMany({}, { $set: { roles: [] } });
+
+    const fs = require('fs');
+
+    fs.readFile('./assets/mocks/domain/events/eventosReales.json', function (err, data) {
+
+      if (err) throw err;
+
+      const books = JSON.parse(data);
+      console.log(books);
+    });
     console.log("ΦΙΝ ");
   } catch (error) {
     console.error("Error en la conexión o en la ejecución:", error);
