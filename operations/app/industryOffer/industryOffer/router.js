935c66a9c99d6093d74cbbdc0802e922b9b0e4db
Carlos <np.carlos@gmail.com>
2026-06-12 16:44:20 -0500

    [FIX] Industry Offer

--- operations/app/industryOffer/industryOffer/router.js
+++ operations/app/industryOffer/industryOffer/router.js
@@ -1,27 +1,31 @@
 var express = require("express");
-const { validateIfUserExists } = require("../../../../helpers");
+const {
+  validateIfUserExists,
+  validateEnvironment,
+} = require("../../../../helpers");
 const apiHelperFunctions = require("../../../../helpers/apiHelperFunctions");
 
 var userRouter = express.Router({ mergeParams: true });
 
 module.exports = [
-  userRouter.get("/", validateIfUserExists, (req, res) => {
+  userRouter.get("/", validateEnvironment, validateIfUserExists, (req, res) => {
     const { role: entityRole } = req.query;
 
+    const lang = req.lang || "es";
     try {
       if (entityRole) {
         const fs = require("fs");
         const content = fs.readFileSync(
-          `./assets/mocks/i18n/${req.lang}/app/industryOffer/${entityRole}.offer.md`,
-          { encoding: "utf8", flag: "r" }
+          `./assets/mocks/i18n/${lang}/app/industryOffer/${entityRole}.offer.md`,
+          { encoding: "utf8", flag: "r" },
         );
         return res.status(200).json(
           apiHelperFunctions.createPaginatedDataResponse({
             content,
-            lang: req.lang,
+            lang: lang,
             version: 1,
             creationDate: 1,
-          })
+          }),
         );
       } else {
         return res.status(404).json({
